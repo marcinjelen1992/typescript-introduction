1771cda88d8f0ef83db8a46ffad5e640a3990c03
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-11-07 21:22:48 +0100

    7thNovember commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+
+  function add2(a, b) {
+    return a + b;
+  }
+  const greet = (name: string): string => {
+    return `Hello ${name}`;
+  };
+
+  console.log(add2(5, 6));
+  // funkcję countCharacters liczącą ilość znaków w stringu
+  const countCharacters = (str: string): number => str.length;
+  // funkcję isEven mówiącą czy liczba jest parzysta czy nie (zwracająca boolean)
+  const isEven = (num: number): boolean => num % 2 === 0;
+  // funkcję getParityMessage która na podstawie boolean zwraca komunikat ta liczba jest parzysta/ta liczba nie jest parzysta
+  function getParityMessage(isEven: boolean): string {
+    return isEven ? "Ta liczba jest parzysta" : "Ta liczba nie jest parzysta";
+  }
+  // funkcję processString która na podstawie stringa, wypisuje go, wypisuje liczbę jego znaków oraz informację czy liczba znaków jest/nie jest parzysta. Funkcja ta powinna wywoływać 3 powyższe funkcje (zwraca string)
+
+  const processString = (str: string): string => {
+    const length = countCharacters(str);
+    const even = isEven(length);
+    const message = getParityMessage(even);
+    return `${str} - liczba znaków ${length} - ${message}`;
+  };
+
+//  const [text, setText] = useState("To jest string")
+
+  // word count
+//  const [wordCount, setWordCount] = useState(0);
+
+  // character count
+//  const [charCount, setCharCount] = useState(0);
+
+// useEffect(() => {
+  // array of words
+//  const words = text.split(' ');
+
+  // update word count
+//  let wordCount = 0;
+//  words.forEach((word) => {
+//    if (word.trim() !== '') {
+//      wordCount++;
+//    }
+//  });
+//  setWordCount(wordCount);
+
+  // update char count (including whitespaces)
+//  setCharCount(text.length);
+// }, [text]);
+
+// console.log(wordCount)
+// console.log(charCount)
+
+// function isEven(input) {
+//  const isEvenNumber = input;
+//    if (isEvenNumber % 2 === 0) {return true} else {return false}
+//  
+// }
+
+// console.log(isEven(7))
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {add2(5, 6)}
+      <p>countCharacters</p>
+      {countCharacters("Hello World!")}
+      <p>isEven</p>
+      {isEven(2) && "true"}
+      {/* To mi się nie wyświetli */}
+      {isEven(3) && "false"}
+      <p>getParityMessage</p>
+      <p>{getParityMessage(true)}</p>
+      <p>{getParityMessage(false)}</p>
+      <p>processString</p>
+      {processString("Hello World")}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
